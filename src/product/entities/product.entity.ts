523d086c7a92aa4562ccb25082d2766102b761cb
Diego Ricardo <diegoricardoweb@gmail.com>
2023-08-12 19:13:39 -0300

    feat: create cart_product entity

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,13 +1,15 @@
-import { CategoryEntity } from '../../category/entities/category.entity';
 import {
   Column,
   CreateDateColumn,
   Entity,
   JoinColumn,
   ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { CartProductEntity } from '../../cart-product/entities/cart-product.entity';
+import { CategoryEntity } from '../../category/entities/category.entity';
 
 @Entity({ name: 'product' })
 export class ProductEntity {
@@ -31,6 +33,9 @@ export class ProductEntity {
 
   @UpdateDateColumn({ name: 'updated_at' })
   updatedAt: Date;
+
+  @OneToMany(() => CartProductEntity, (cartProduct) => cartProduct.product)
+  cartProduct?: CartProductEntity[];
 
   @ManyToOne(() => CategoryEntity, (category) => category.products)
   @JoinColumn({ name: 'category_id', referencedColumnName: 'id' })
