941000890ce744c313e548b49bece31cf38a0524
Diego Ricardo <diegoricardoweb@gmail.com>
2023-08-12 21:43:14 -0300

    feat: create cart of user

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,7 +1,28 @@
-import { Controller } from '@nestjs/common';
-import { Roles } from 'src/decorators/role.decorator';
-import { UserType } from 'src/user/enum/user-type.enum';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { UserId } from 'src/decorators/user-id.decorator';
+import { Roles } from '../decorators/role.decorator';
+import { UserType } from '../user/enum/user-type.enum';
+import { CartService } from './cart.service';
+import { InsertCartDTO } from './dtos/insert-cart.dto';
+import { CartEntity } from './entities/cart.entity';
 
-@Roles(UserType.User)
+@Roles(UserType.User, UserType.Admin)
 @Controller('cart')
-export class CartController {}
+export class CartController {
+  constructor(private readonly cartService: CartService) {}
+
+  @UsePipes(ValidationPipe)
+  @Post()
+  async createCart(
+    @Body() insertCart: InsertCartDTO,
+    @UserId() userId: number,
+  ): Promise<CartEntity> {
+    return this.cartService.insertProductInCart(insertCart, userId);
+  }
+}
