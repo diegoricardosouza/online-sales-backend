84bca57aedd524d1d4aa21cc05cfab3ac0c05c31
Diego Ricardo <diegoricardoweb@gmail.com>
2023-08-26 00:20:02 -0300

    feat: insert product in cart

--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CartProductModule } from '../cart-product/cart-product.module';
 import { CartController } from './cart.controller';
 import { CartService } from './cart.service';
 import { CartEntity } from './entities/cart.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CartEntity])],
+  imports: [TypeOrmModule.forFeature([CartEntity]), CartProductModule],
   providers: [CartService],
   controllers: [CartController],
 })
